063a25015838941a9751fa5595e2b396f61def38
parameswari-sampath <parameswarisampathkumar75@gmail.com>
2025-05-07 14:42:37 +0530

    added remaining Operations

--- src/main/java/com/dharani/demo/controller/RoomController.java
+++ src/main/java/com/dharani/demo/controller/RoomController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rooms")
@@ -31,6 +32,44 @@ public class RoomController {
             return ResponseEntity.
                     status(500).
                     body(Map.of("message", "Error creating room"));
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllRooms() {
+        return ResponseEntity.ok(roomRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getRoomById(@PathVariable String id) {
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+        if (optionalRoom.isPresent()) {
+            return ResponseEntity.ok(optionalRoom.get());
+        } else {
+            return ResponseEntity.status(404).body(Map.of("message", "Room not found"));
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateRoom(@PathVariable String id, @RequestBody Room updatedRoom) {
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+        if (optionalRoom.isPresent()) {
+            updatedRoom.setId(id);
+            roomRepository.save(updatedRoom);
+            return ResponseEntity.ok(Map.of("message", "Room updated successfully"));
+        } else {
+            return ResponseEntity.status(404).body(Map.of("message", "Room not found"));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRoom(@PathVariable String id) {
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+        if (optionalRoom.isPresent()) {
+            roomRepository.deleteById(id);
+            return ResponseEntity.ok(Map.of("message", "Room deleted successfully"));
+        } else {
+            return ResponseEntity.status(404).body(Map.of("message", "Room not found"));
         }
     }
 }
